0ea5c67d0f0371b8afc2ec64489b4886bf8e3bae
Robert Gordon <rjgcustoms@hotmail.com>
2025-03-31 00:31:36 -0400

    Update decryptAES2.py
    working version that will decrypt master and private keys

--- decryptAES2.py
+++ decryptAES2.py
@@ -2,7 +2,7 @@ import hashlib
 import binascii
 import pyaes
 
-password = b"password"
+password = b"passphrase"
 salt = binascii.unhexlify("salt in hex")
 iterations = iteration count  # Example: 200000
 encrypted_master_key = binascii.unhexlify("encrypted master key")
@@ -13,7 +13,7 @@ for i in range(iterations):
 
 aes_key = derived_key[:32]
 
-aes_iv = derived_key[:16]  # First 16 bytes are IV
+aes_iv = derived_key[32:32+16]
 encrypted_data = encrypted_master_key
 
 decrypter = pyaes.Decrypter(pyaes.AESModeOfOperationCBC(aes_key, aes_iv))
@@ -21,3 +21,14 @@ decryptedData = decrypter.feed(encrypted_master_key)
 decryptedData += decrypter.feed()
 
 print(binascii.hexlify(decryptedData))
+
+publicKey = binascii.unhexlify("public key")
+encrypted_priv_Key = (binascii.unhexlify("encrypted private key"))
+master_Key = decryptedData[0:32]
+newIV = (hashlib.sha256(hashlib.sha256(publicKey).digest()).digest())[0:16]
+
+decrypter2 = pyaes.Decrypter(pyaes.AESModeOfOperationCBC(master_Key, newIV))
+decryptedData2 = decrypter2.feed(encrypted_priv_Key)
+decryptedData2 += decrypter2.feed()
+
+print(binascii.hexlify(decryptedData2))
